eb49e65b1a428b179b3ddd951554e107cb49b8ea
jeffrey.chee@yahoo.com <jeffrey.chee@yahoo.com>
2018-12-05 12:57:32 -0800

    changes to heroku pathway

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var path = require('path');
 var app = express();
 
 
-app.use(express.static('assets'));
+app.use(express.static('public'));
 
 var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 
